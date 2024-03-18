f48c112c924769cbef9f2b0751dcf4f19e16e188
Dmitriy Sharipov <dmsharipov00@gmail.com>
2024-03-19 00:33:46 +0300

    Feat: подсчет статистик оконной функцией

--- SKPredModel.py
+++ SKPredModel.py
@@ -44,6 +44,13 @@ class SKPredModel:
                     return (int(rsearch.group('days')) * 24 + int(rsearch.group('hours'))) * 3600 + int(rsearch.group('minutes')) * 60 + int(rsearch.group('seconds'))
                 except:
                     return int(rsearch.group('hours')) * 3600 + int(rsearch.group('minutes')) * 60 + int(rsearch.group('seconds'))
+                
+    def calculate_features(self, group):
+        group['mean_elapsed'] = group['Elapsed'].expanding(min_periods=1).mean()
+        group['succeded_task_count'] = group['State'].expanding(min_periods=group.shape[0]//100).apply(lambda x: (x == 0).sum())
+        group['failed_task_count'] = group['State'].expanding(min_periods=group.shape[0]//100).apply(lambda x: (x == 1).sum())
+        group['timeout_task_count'] = group['State'].expanding(min_periods=group.shape[0]//100).apply(lambda x: (x == 2).sum())
+        return group
 
     def prepare_df(self, test_df: pd.DataFrame) -> pd.DataFrame:
         """
@@ -80,41 +87,26 @@ class SKPredModel:
             'NODE_FAIL': 3,
             'OUT_OF_MEMORY': 4
         }, inplace=True)
-        # test_df['State'].replace(r'(CANCELLED.+)|(CANCELLED)', 5, regex=True, inplace=True)
-        # test_df['State'].astype(int)
-        # test_df['ExitCode'].replace(r':','', regex=True, inplace=True)
-        # test_df['ExitCode'] = test_df.ExitCode.astype(int)
-        # test_df['ExitCode'].astype(int)
+        test_df['State'].replace(r'(CANCELLED.+)|(CANCELLED)', 5, regex=True, inplace=True)
+        test_df['State'].astype(int)
+        test_df['ExitCode'].replace(r':','', regex=True, inplace=True)
+        test_df['ExitCode'] = test_df.ExitCode.astype(int)
+        test_df['ExitCode'].astype(int)
         test_df['Submit'] = pd.to_datetime(test_df['Submit'])
         test_df['Month'] = test_df['Submit'].dt.month
-        test_df.drop(columns=['Submit'], inplace=True)
+
+        # Добавление фичей
+        test_df.sort_values(by=['UID', 'Submit'], inplace=True)
+        test_df = test_df.groupby('UID').apply(self.calculate_features)
 
         ### Апостериорно получаемые фичи
         test_df.drop(columns=['State'], inplace=True)
         test_df.drop(columns=['ExitCode'], inplace=True)
-
+    
         test_df.drop(columns=['Elapsed'], inplace=True)
+        test_df.drop(columns=['Submit'], inplace=True)
 
         test_df.reset_index(inplace=True, drop=True)
-
-        # mean_elapsed = pd.DataFrame(test_df[test_df['State'] == 0].groupby('UID').mean()['Elapsed'])
-        # mean_elapsed.columns = ['mean_elapsed']
-        # mean_elapsed.reset_index(inplace=True)
-        # test_df = test_df.merge(mean_elapsed, on='UID', how='left')
-        # succeded_task_count = pd.DataFrame(test_df[test_df['State'] == 0].groupby('UID').count()['State'])
-        # succeded_task_count.columns = ['succeded_task_count']
-        # succeded_task_count.reset_index(inplace=True)
-        # failed_task_count = pd.DataFrame(test_df[test_df['State'] == 1].groupby('UID').count()['State'])
-        # failed_task_count.columns = ['failed_task_count']
-        # failed_task_count.reset_index(inplace=True)
-        # timeout_task_count = pd.DataFrame(test_df[test_df['State'] == 2].groupby('UID').count()['State'])
-        # timeout_task_count.columns = ['timeout_task_count']
-        # timeout_task_count.reset_index(inplace=True)
-        # tasks = succeded_task_count.merge(failed_task_count, on='UID', how='left')
-        # tasks = tasks.merge(timeout_task_count, on='UID', how='left')
-        # tasks.fillna({'succeded_task_count': 0, 'failed_task_count': 0, 'timeout_task_count': 0}, inplace=True)
-        # tasks['succeded_task_proportion'] = tasks['succeded_task_count'] / (tasks['succeded_task_count'] + tasks['failed_task_count'] + tasks['timeout_task_count'])
-        # test_df = test_df.merge(tasks, on='UID', how='left')
 
         return test_df
     
