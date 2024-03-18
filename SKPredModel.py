e1425f983598c825c13a75e075875dbfc14a8bb1
Dmitriy Sharipov <dmsharipov00@gmail.com>
2024-03-18 16:36:21 +0300

    Fix: использование апостериорно получаемыех фичей

--- SKPredModel.py
+++ SKPredModel.py
@@ -79,33 +79,39 @@ class SKPredModel:
             'NODE_FAIL': 3,
             'OUT_OF_MEMORY': 4
         }, inplace=True)
-        test_df['State'].replace(r'(CANCELLED.+)|(CANCELLED)', 5, regex=True, inplace=True)
-        test_df['State'].astype(int)
-        test_df['ExitCode'].replace(r':','', regex=True, inplace=True)
-        test_df['ExitCode'] = test_df.ExitCode.astype(int)
-        test_df['ExitCode'].astype(int)
-        test_df.reset_index(inplace=True, drop=True)
+        # test_df['State'].replace(r'(CANCELLED.+)|(CANCELLED)', 5, regex=True, inplace=True)
+        # test_df['State'].astype(int)
+        # test_df['ExitCode'].replace(r':','', regex=True, inplace=True)
+        # test_df['ExitCode'] = test_df.ExitCode.astype(int)
+        # test_df['ExitCode'].astype(int)
         test_df['Submit'] = pd.to_datetime(test_df['Submit'])
         test_df['Month'] = test_df['Submit'].dt.month
         test_df.drop(columns=['Submit'], inplace=True)
-        mean_elapsed = pd.DataFrame(test_df[test_df['State'] == 0].groupby('UID').mean()['Elapsed'])
-        mean_elapsed.columns = ['mean_elapsed']
-        mean_elapsed.reset_index(inplace=True)
-        test_df = test_df.merge(mean_elapsed, on='UID', how='left')
-        succeded_task_count = pd.DataFrame(test_df[test_df['State'] == 0].groupby('UID').count()['State'])
-        succeded_task_count.columns = ['succeded_task_count']
-        succeded_task_count.reset_index(inplace=True)
-        failed_task_count = pd.DataFrame(test_df[test_df['State'] == 1].groupby('UID').count()['State'])
-        failed_task_count.columns = ['failed_task_count']
-        failed_task_count.reset_index(inplace=True)
-        timeout_task_count = pd.DataFrame(test_df[test_df['State'] == 2].groupby('UID').count()['State'])
-        timeout_task_count.columns = ['timeout_task_count']
-        timeout_task_count.reset_index(inplace=True)
-        tasks = succeded_task_count.merge(failed_task_count, on='UID', how='left')
-        tasks = tasks.merge(timeout_task_count, on='UID', how='left')
-        tasks.fillna({'succeded_task_count': 0, 'failed_task_count': 0, 'timeout_task_count': 0}, inplace=True)
-        tasks['succeded_task_proportion'] = tasks['succeded_task_count'] / (tasks['succeded_task_count'] + tasks['failed_task_count'] + tasks['timeout_task_count'])
-        test_df = test_df.merge(tasks, on='UID', how='left')
+
+        ### Апостериорно получаемые фичи
+        test_df.drop(columns=['State'], inplace=True)
+        test_df.drop(columns=['ExitCode'], inplace=True)
+
+        test_df.reset_index(inplace=True, drop=True)
+
+        # mean_elapsed = pd.DataFrame(test_df[test_df['State'] == 0].groupby('UID').mean()['Elapsed'])
+        # mean_elapsed.columns = ['mean_elapsed']
+        # mean_elapsed.reset_index(inplace=True)
+        # test_df = test_df.merge(mean_elapsed, on='UID', how='left')
+        # succeded_task_count = pd.DataFrame(test_df[test_df['State'] == 0].groupby('UID').count()['State'])
+        # succeded_task_count.columns = ['succeded_task_count']
+        # succeded_task_count.reset_index(inplace=True)
+        # failed_task_count = pd.DataFrame(test_df[test_df['State'] == 1].groupby('UID').count()['State'])
+        # failed_task_count.columns = ['failed_task_count']
+        # failed_task_count.reset_index(inplace=True)
+        # timeout_task_count = pd.DataFrame(test_df[test_df['State'] == 2].groupby('UID').count()['State'])
+        # timeout_task_count.columns = ['timeout_task_count']
+        # timeout_task_count.reset_index(inplace=True)
+        # tasks = succeded_task_count.merge(failed_task_count, on='UID', how='left')
+        # tasks = tasks.merge(timeout_task_count, on='UID', how='left')
+        # tasks.fillna({'succeded_task_count': 0, 'failed_task_count': 0, 'timeout_task_count': 0}, inplace=True)
+        # tasks['succeded_task_proportion'] = tasks['succeded_task_count'] / (tasks['succeded_task_count'] + tasks['failed_task_count'] + tasks['timeout_task_count'])
+        # test_df = test_df.merge(tasks, on='UID', how='left')
 
         return test_df
 
@@ -116,6 +122,9 @@ class SKPredModel:
         :param test_df: dataframe to predict
         :return: vector of estimated times in milliseconds
         """
+        for key in  self.regressor.get_booster().feature_names:
+            assert key in test_df.keys(), f"{key} column missed in test_df"
+
         predictions = self.regressor.predict(test_df)
         
         return predictions
